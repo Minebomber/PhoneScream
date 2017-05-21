7aedea474cd41965dab4e8ec10a6b592c12b9a0d
HsHack <CodeDay@e1z1r4p3.42.us.org>
2017-05-21 08:19:48 -0700

    Initial and Final Commit

--- KEKKEKEKKEKEK/ViewController.swift
+++ KEKKEKEKKEKEK/ViewController.swift
@@ -7,12 +7,60 @@
 //
 
 import UIKit
+import CoreMotion
+import AVKit
+import AVFoundation
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var faceView: UIImageView!
+    
+    let threshold = 1.55
+    
+    var shouldScream = false
+    
+    var motionManager: CMMotionManager!
+    
+    var player: AVAudioPlayer?
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        
+        let screamPath = Bundle.main.path(forResource: "scream", ofType: "aiff")
+        let screamUrl = URL(fileURLWithPath: screamPath!)
+        
+        do { player = try AVAudioPlayer(contentsOf: screamUrl) } catch {}
+        player?.volume = 1.0
+        player?.numberOfLoops = -1
+        var timeout = 0
+        
+        motionManager = CMMotionManager()
+        motionManager.startAccelerometerUpdates(to: .main) { (data, error) in
+            
+            if (self.shouldScream) {
+                if !(self.player?.isPlaying)! {self.player?.play()}
+                self.faceView.image = #imageLiteral(resourceName: "screaming")
+            } else {
+                self.player?.pause()
+                self.faceView.image = #imageLiteral(resourceName: "normal")
+            }
+            
+            if ((data!.acceleration.x < -self.threshold) || (data!.acceleration.y < -self.threshold) || (data!.acceleration.z < -self.threshold)) {
+                // falling
+                
+                timeout = 0
+                
+            } else {
+                timeout += 1
+            }
+            
+            self.shouldScream = timeout < 10
+            NSLog(String(self.shouldScream))
+            
+            
+        }
+        
     }
 
     override func didReceiveMemoryWarning() {
